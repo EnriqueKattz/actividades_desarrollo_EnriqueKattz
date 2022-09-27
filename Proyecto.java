5c04c4aaff17a0b3a1e8a9288002c2468c27f34e
EnriqueKattz <100949666+EnriqueKattz@users.noreply.github.com>
2022-09-26 20:08:39 -0500

    Delete Proyecto.java

--- Proyecto.java
+++
@@ -1,7 +0,0 @@
-package com.mycompany.proyecto;
-public class Proyecto {
-    public static void main(String[] args) {
-        Sistema ventana = new Sistema();
-        ventana.setVisible(true);
-    }
-}
